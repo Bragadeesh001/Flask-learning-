7c6e90d04f214a54465fda1919eec56c92249629
Bragadeesh S <bragadeeshs75@gmail.com>
2022-08-22 01:58:25 -0700

    Add files via upload

---
+++ Integrating HTML/app.py
@@ -0,0 +1,37 @@
+#import required libraries
+from flask import Flask,render_template,url_for,redirect
+from flask import Flask, request
+
+#create the opbject
+app=Flask(__name__)
+
+#index page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Action form
+@app.route('/submit',methods=["POST","GET"])
+def submit():
+    total_score=0
+    if request.method=="POST":
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        english=float(request.form['english'])
+        computer=float(request.form['computer science'])
+        total_score=(science+maths+english+computer)/4
+    
+    return redirect(url_for('result',score=total_score))
+
+#result url
+@app.route('/result/<int:score>')
+def result(score):
+    if score>=50:
+        res='pass'
+    else:
+        res='fail'
+    return render_template('result.html',results=res)
+
+#to run
+if __name__=='__main__':
+    app.run(debug=True)
